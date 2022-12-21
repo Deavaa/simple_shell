ea2ff324cec3d95152b9e4cac46ea6946d37a698
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-21 09:10:01 -0800

    path

---
+++ path.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * find_p - finds the path
+ * Return: path
+ */
+
+char *find_path(void)
+{
+	int x;
+	char **env = environ, *path = NULL;
+
+	while (*env)
+	{
+		if (_strncmp(*env, "PATH=", 5) == 0)
+		{
+			path = *env;
+			while (*path && x < 5)
+			{
+				path++;
+				x++;
+			}
+			return (path);
+		}
+		env++;
+	}
+	return (NULL);
+}
