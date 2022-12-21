fe90b6c5094cc8679a73d6d46d4dfb92aa6c5232
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-21 09:52:33 -0800

    builtin

---
+++ handle_buildin.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * handle_builtin - handles execution of builtin function
+ * @com: tokenized command
+ * @rd: input read
+ * Return: 1 if executed, 0 if not
+ */
+
+int handle_builtin(char **com, char *rd)
+{
+	struct builtin builtin = {"env", "exit"};
+
+	if (_strcmp(*command, builtin.env) == 0)
+	{
+		print_env();
+		return (1);
+	}
+	else if (_strcmp(*command, builtin.exit) == 0)
+	{
+		exit_cmd(command, line);
+		return (1);
+	}
+	return (0);
+}
