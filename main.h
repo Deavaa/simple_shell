0cf3a954064eb1f88c3412534b3b97b1bb01dfee
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-23 09:35:41 -0800

    fun

--- main.h
+++ main.h
@@ -24,6 +24,20 @@ char **tokenizer(char *line);
 char *test_path(char **path, char *command);
 char *append_path(char *path, char *command);
 int handle_builtin(char **cmd, char *rd);
+
+void exit_cmd(char **command, char *line);
+void print_env(void);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+void execution(char *cp, char **cmd);
+char *find_path(void);
+void free_buffers(char **buf);
+
+
+
 struct builtin
 {
 	char *env;
