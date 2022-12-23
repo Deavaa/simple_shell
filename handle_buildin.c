ad6181c2eb26beb553d6cfebd97ade61935f959b
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-23 09:41:36 -0800

    fun

--- handle_buildin.c
+++ handle_buildin.c
@@ -7,19 +7,19 @@
  * Return: 1 if executed, 0 if not
  */
 
-int handle_builtin(char **com, char *rd)
+int handle_builtin(char **command, char *line)
 {
-	struct builtin builtin = {"env", "exit"};
+		struct builtin builtin = {"env", "exit"};
 
-	if (_strcmp(*com, builtin.env) == 0)
-	{
-		print_env();
-		return (1);
-	}
-	else if (_strcmp(*com, builtin.exit) == 0)
-	{
-		exit_cmd(com, rd);
-		return (1);
-	}
-	return (0);
+		if (_strcmp(*command, builtin.env) == 0)
+		{
+			print_env();
+			return (1);
+		}
+		else if (_strcmp(*command, builtin.exit) == 0)
+		{
+			exit_cmd(command, line);
+			return (1);
+		}
+		return (0);
 }
