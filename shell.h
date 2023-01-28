2d7c4849f921b3b3826f6fc0def88c1f4e7d7bdf
Dinobi <architectstan01@gmail.com>
2023-01-28 06:21:26 -0800

    update

--- shell.h
+++ shell.h
@@ -24,6 +24,7 @@ extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 int checker(char **cmd, char *buf);
 void prompt_user(void);
 void handle_signal(int m);
+int get_help(data_shell *datash);
 char **tokenizer(char *line);
 char *test_path(char **path, char *command);
 char *append_path(char *path, char *command);
@@ -32,10 +33,22 @@ void exit_cmd(char **command, char *line);
 int _setenv(data_shell *datash);
 int _unsetenv(data_shell *datash);
 int exit_shell(data_shell *datash);
-void print_env(void)
+void print_env(void);
 int _unsetenv(data_shell *datash);
 int _setenv(data_shell *datash);
 void set_env(char *name, char *value, data_shell *datash);
+char *swap_char(char *input, int bool);
+void add_nodes(sep_list **head_s, line_list **head_l, char *input);
+void go_next(sep_list **list_s, line_list **list_l, data_shell *datash);
+int split_commands(data_shell *datash, char *input);
+char **split_line(char *input);
+/* cd.c */
+void cd_dot(data_shell *datash);
+void cd_to(data_shell *datash);
+void cd_previous(data_shell *datash);
+void cd_to_home(data_shell *datash);
+/* _exit.c */
+int exit_shell(data_shell *datash);
 char *copy_info(char *name, char *value);
 /* string handlers */
 int _strcmp(char *s1, char *s2);
@@ -70,6 +83,29 @@ struct info
 	int final_exit;
 	int ln_count;
 } info;
+/**
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: next node
+ * Description: single linked list to store separators
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct line_list_s - single linked list
+ * @line: command line
+ * @next: next node
+ * Description: single linked list to store command lines
+ */
+typedef struct line_list_s
+{
+	char *line;
+	struct line_list_s *next;
+} line_list;
 
 struct flags
 {
